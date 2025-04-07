c92b7e49055ffb0596dfe7f2e7f564aa9d764f42
Wander <wander.bicalho@feedz.com.br>
2025-04-07 14:55:17 -0300

    oo basic

---
+++ src/oo_basic3.ts
@@ -0,0 +1,37 @@
+class Address {
+    constructor(
+        public address: string,
+        public zipCode: string,
+        public number?: number
+    ) {}
+  }
+  
+  export class User {
+    name: string
+    email: string
+    address: Address[] = []
+    active: boolean = false
+  
+    constructor(name: string, email: string, active: boolean = false) {
+        this.name = name
+        this.email = email
+        this.active = active
+    }
+  
+    addAddress(newAddress: Address): void {
+        this.address.push(newAddress)
+    }
+  
+    getAddress(): Address[] {
+        return this.address
+    }
+  } 
+  
+  const address1 = new Address('Rua x', '75702-050', 187);
+  const address2 = new Address('Rua y', '75702-051', 188);
+  const user1 = new User('User 01', 'user1@email.com')
+  console.log(user1)
+
+  user1.addAddress(address1)
+  user1.addAddress(address2)
+  console.log(user1)
